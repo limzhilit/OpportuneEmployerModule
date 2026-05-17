0fec31bad395ac53b3c2465753f128f11986a8a6
limzhilit <g00440629@atu.ie>
2026-05-17 16:58:29 +0100

    remove unused import statement from EmployerService.java

--- src/main/java/ie/atu/employer/Service/EmployerService.java
+++ src/main/java/ie/atu/employer/Service/EmployerService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
